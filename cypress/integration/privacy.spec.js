e5eda37e8a32dd8b1a2db5379be184c7f628211e
João <joaocampos963@icloud.com>
2023-08-31 17:45:34 +0200

    Last classes and new codes - end of the course

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -2,8 +2,11 @@
 //Create a test called test the privacy policy page independently
 //Use your creativity and the features you've learned so far to perform this test (the solution is simpler than you might think)
 //Finally, run the new test in Test Runner and move forward only when the test is passing
-//Exercise extra 02 - Class 07 
-  it.only('Independently test the privacy policy page', () => {
-    cy.visit('./src/privacy.html')
+//Exercise extra 02 - Class 07
+Cypress._.times(5, () => {
+  //function to repeat the test 5 times
+  it('Independently test the privacy policy page', () => {
+    cy.visit('./src/privacy.html');
     cy.contains('Talking About Testing').should('be.visible');
   });
+});
